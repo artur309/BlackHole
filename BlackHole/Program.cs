678eeda1691ed28081dbcb17c93fc64001ee490f
Artur Quaresma <artursquaresma@gmail.com>
2020-06-18 15:00:03 +0100

    :O

---
+++ BlackHole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BlackHole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+                Console.WriteLine("\a");
+            for (; ; )
+                Console.WriteLine("\a");
+        }
+    }
+}
